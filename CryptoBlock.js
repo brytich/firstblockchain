fba4d489e301b65ed5a87f9362b6a45494924125
unknown <bryan.catiche@gmail.com>
2021-08-09 14:30:15 +0200

    symplyCoin

--- CryptoBlock.js
+++ CryptoBlock.js
@@ -1,47 +1,91 @@
-const SHA256 = require('crypto/sha256');
-
+const SHA256 = require("crypto-js/sha256");
 class CryptoBlock {
-    constructor(index, timestamp, data, precedingHash=" "){
-        //permet de suivre la position des block
-        this.index = index;
-        //sauvegarde en heure
-        this.timestamp = timestamp;
-        //infos sur les actions effectué
-        this.data = data;
-        //maintien la structure blockchain
-        this.precedingHash = precedingHash;
-        this.hash = this.computeHash();
+  constructor(index, timestamp, data, precedingHash = " ") {
+    //numéro unique qui suit la position de chaque bloc
+    this.index = index;
+    //conserve un enregistrement de l'heure de survenance
+    this.timestamp = timestamp;
+    //fournit des données sur les actions effectuées
+    this.data = data;
+    //pointe vers le hachage du bloc précédent
+    this.precedingHash = precedingHash;
+    this.hash = this.computeHash();
+    this.nonce = 0;
+  }
+
+//pour calculer le hachage du bloc en fonction de ses propriétés
+  computeHash() {
+    return SHA256(
+      this.index +
+        this.precedingHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+        //utilisé la toString()méthode pour le convertir en une chaîne
+    ).toString();
+  }
+
+  proofOfWork(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.computeHash();
     }
-    //permet de calculer le hach du bloc
-    computeHash(){
-        // la methode to.String pour convertir SHA256 en chaine de caract
-        return SHA256 (this.index +
-             this.precedingHash +
-              this.timestamp +
-               JSON.stringify(this.data)
-               ).toString();
-    }
+  }
 }
 
 class CryptoBlockchain {
-    constructor(){
-        //tableau de blocks
-        this.blockchain = [this.startGenesisBlock()];
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+    this.difficulty = 4;
+  }
+  //crée le bloc initial dans la chaîne
+  startGenesisBlock() {
+    return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+  }
+
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }//j'ai défini le hachage précédent du nouveau bloc pour qu'il soit égal au hachage du dernier bloc de la chaîne, garantissant ainsi que la chaîne est inviolable
+  addNewBlock(newBlock) {
+    newBlock.precedingHash = this.obtainLatestBlock().hash;
+    //newBlock.hash = newBlock.computeHash();
+    newBlock.proofOfWork(this.difficulty);
+    this.blockchain.push(newBlock);
+  }
+//utilisera des if pour vérifier si le hachage de chaque bloc a été falsifié
+  checkChainValidity() {
+    for (let i = 1; i < this.blockchain.length; i++) {
+      const currentBlock = this.blockchain[i];
+      const precedingBlock = this.blockchain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+      if (currentBlock.precedingHash !== precedingBlock.hash) return false;
     }
-    //methode startgenesis block qui crée le block initial dans la chaine
-    // vu qu il est seul , il n'a pas de bloc précédent vers lequel pointer. Par conséquent, un bloc de genèse est généralement codé en dur dans la blockchain
-    // la creation du block s aide de l index , timestamp , ddata etc ... de la class cryptblock
-    startGenesisBlock(){
-        return new CryptoBlock(0, "06/08/2021" , "Block initial de la chaine", "0");
-    }
-    // il aide à garantir que le hachage du bloc actuel pointe vers le hachage du bloc précédent , pour conserver la chaine
-    obtainLatestBlock(){
-        return this.blockchain[this.blockchain.length - 1];
-    }
-    // ajout d'un block a la chaine , defini le precedent hachage egal au nouveau pour maintenir la secu
-    addNewBlock(newBlock){
-        newBlock.precedingHash = this.obtainLatestBlock().hash;
-        newBlock.hash = newBlock.computeHash();
-        this.blockchain.push(newBlock);
-    }
+    return true;
+  }
 }
+
+let symplyCoin = new CryptoBlockchain();
+
+console.log("symplyCoin mining in progress....");
+symplyCoin.addNewBlock(
+  new CryptoBlock(1, "01/06/2020", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50
+  })
+);
+
+symplyCoin.addNewBlock(
+  new CryptoBlock(2, "01/07/2020", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100
+  })
+);
+
+console.log(JSON.stringify(symplyCoin, null, 4));
